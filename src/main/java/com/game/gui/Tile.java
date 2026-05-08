5d37fc8261e968b9b76abd1fc230e427e69c4993
alexf <alex.fruth.m@gmail.com>
2026-05-07 22:10:01 -0400

    tile constructer changes and min ratios

--- src/main/java/com/game/gui/Tile.java
+++ src/main/java/com/game/gui/Tile.java
@@ -3,17 +3,25 @@ package com.game.gui;
 import javafx.scene.image.Image;
 
 public class Tile {
-    public final Image image;
+    public Image image;
+    public boolean isCollidable;
 
-    public static final Tile TEST = new Tile("/test/test_tile.png");
-    public static final Tile RENDER = new Tile("/test/render_tile.png");
-    public static final Tile STONE1 = new Tile("/test/stone_1.png");
-    public static final Tile STONE2 = new Tile("/test/stone_2.png");
-    public static final Tile STONE3 = new Tile("/test/stone_3.png");
+    public static final Tile TEST = new Tile("/test/test_tile.png", false);
+    public static final Tile RENDER = new Tile("/test/render_tile.png", false);
 
-    public static final Tile START_GRID = new Tile("/screens/start/grid.png");
+    public static final Tile STONE1 = new Tile("/test/stone_1.png", false);
+    public static final Tile STONE2 = new Tile("/test/stone_2.png", false);
+    public static final Tile STONE3 = new Tile("/test/stone_3.png", false);
 
-    public Tile(String path) {
+    public static final Tile START_GRID = new Tile("/screens/start/grid.png", false);
+
+    public Tile(String path, boolean isCollidable) {
         this.image = new Image(getClass().getResourceAsStream(path));
+        this.isCollidable = isCollidable;
     }
+
+    public static boolean isCollidable(Tile tile) {
+        return tile.isCollidable;
+    }
+
 }
